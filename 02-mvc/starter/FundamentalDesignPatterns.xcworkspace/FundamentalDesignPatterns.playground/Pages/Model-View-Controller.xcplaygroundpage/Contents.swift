48142cdbc27cf1fd39d7975ee4fb7dee6f3d8897
Raiyan <Raiyan-sharif@users.noreply.github.com>
2023-05-30 17:26:45 +0600

    Latest practice

--- 02-mvc/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
+++ 02-mvc/starter/FundamentalDesignPatterns.xcworkspace/FundamentalDesignPatterns.playground/Pages/Model-View-Controller.xcplaygroundpage/Contents.swift
@@ -36,11 +36,43 @@ public final class AddressView: UIView{
 
 // MARK: - Controller
 public final class AddressController: UIViewController{
-    public var address: Address?
+    public var address: Address?{
+        didSet{
+            updateViewFromAddress()
+        }
+    }
     public var addressView: AddressView!{
         guard isViewLoaded else {return nil}
         return (view as! AddressView)
     }
 
     // MARK: - View Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        updateViewFromAddress()
+    }
+
+    private func updateViewFromAddress(){
+        guard let addressView = addressView, let address = address else{ return }
+        addressView.streetTextField.text = address.street
+        addressView.cityTextField.text = address.city
+        addressView.stateTextField.text = address.state
+        addressView.zipCodeTextField.text = address.zipCode
+    }
+
+    // MARK: - Action
+    @IBAction public func updateAddressFromView(_ sender: AnyObject){
+        guard let street = addressView.streetTextField.text,
+              street.count > 0,
+              let city = addressView.cityTextField.text,
+              city.count > 0,
+              let state = addressView.stateTextField.text,
+              state.count > 0,
+              let zipCode = addressView.zipCodeTextField.text,
+              zipCode.count > 0 else{
+            // TODO: - Handle the error, show an error message, etc
+            return
+        }
+        address = Address(street: street, city: city, state: state, zipCode: zipCode)
+    }
 }
